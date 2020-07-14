b3fcd61c98eceb587bfea0019b4e885f6eb6bda2
Negar Lajevardi <negarlajevardi4@gmail.com>
2020-07-13 21:10:21 -0700

    reviewing

---
+++ Practice/reviewing.cpp
@@ -0,0 +1,27 @@
+//Session-4
+#include <iostream>
+
+int main()
+{
+    std::cout << "reviewing lessons 1-7\n";
+
+    //Session-6
+
+    int A = 1000;
+    short B = 10;
+    int C;
+    C = A - B;
+
+    std::cout << "C = " << C << std::endl;
+
+    //Session-7
+    double step;
+
+    unsigned short sh;
+    sh = 256 + 75;
+    unsigned char ch = sh;
+
+    std::cout << "sh: " << sh << std::endl;
+    std::cout << "ch: " << ch << std::endl;
+    return 0;
+}
