4851e48acb8514e5cf62ad5f0833a07ced4187ab
Negar Lajevardi <negarlajevardi4@gmail.com>
2020-07-16 13:19:50 -0700

    escape characters

---
+++ Practice/escapecharacters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "escape characters\n";
+    std::cout << "random:\n";
+    std::cout << "\tddddd\n";
+    std::cout << "\t\"Fancy\"\n";
+    std::cout << "\t\\Food\\\n";
+    std::cout << "\tHello\?\n";
+    std::cout << "\v\tHelloooo\?\n";
+    return 0;
+}
