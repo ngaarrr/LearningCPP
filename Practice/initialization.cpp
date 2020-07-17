94775baae8c11a6f049ffa5f6166661911316830
Negar Lajevardi <negarlajevardi4@gmail.com>
2020-07-16 21:10:18 -0700

    initialization

---
+++ Practice/initialization.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int a = {55};
+    int b = {2};
+    int d = 4;
+    int e(5);
+
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+    std::cout << "d: " << d << std::endl;
+    std::cout << "e: " << e << std::endl;
+    return 0; 
+}
