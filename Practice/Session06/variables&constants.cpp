74a90960d4b31083c9198f332ec4ff2668ed68d7
Negar Lajevardi <negarlajevardi4@gmail.com>
2020-08-10 22:16:06 -0700

    Session 06 practices

---
+++ Practice/Session06/variables&constants.cpp
@@ -0,0 +1,22 @@
+int main()
+{
+    //initializing variables (can assign a value later)
+    int Var = 0; //datatype identifier = value
+    int Var2{1};
+
+    //Case-sensitive
+    int var = 0;
+    int var2{1};
+
+    //int 2var = 0; can't use
+    //int long = 0; reserved words cant's be used as identifier
+    return 0;
+
+    /* contants (can't assign variable later)
+    use eveywhere you can
+    allows for less errors */
+
+    const int x = 10;
+    int const y = 20;
+    //y = 30; can't change value
+}
